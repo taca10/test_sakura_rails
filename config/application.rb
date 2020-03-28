9e4a2b46a1ca71c428dd4dcdd8332002b60a6cf6
坪内崇人 <taka.ur.0505@gmail.com>
2020-03-28 17:26:28 +0900

    アクセスしたいホスト名を登録

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module TestSakuraRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.hosts << 'choli.org'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
