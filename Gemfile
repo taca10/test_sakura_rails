9b79d32ae993bb3c1932eb143ad749b759bab56e
坪内崇人 <taka.ur.0505@gmail.com>
2020-04-04 16:16:35 +0900

    sakuravpsでzozoにスクレイピングできるか実験

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'unicorn'
+gem "mechanize"
+gem 'slack-ruby-client'
--- app/controllers/slackbots_controller.rb
+++ app/controllers/slackbots_controller.rb
@@ -1,5 +1,5 @@
 class SlackbotsController < ApplicationController
   def test
-
+    ZozoScraping.fetch_wear_page("https://wear.jp/men-category/tops/knit-sweater/")
   end
 end
