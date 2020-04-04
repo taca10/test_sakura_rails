141d91e2968141392e1a8658c3da63e59331cd01
坪内崇人 <taka.ur.0505@gmail.com>
2020-04-04 17:17:55 +0900

    config/applicationファイル修正

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,12 @@ module TestSakuraRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = "Tokyo"
 
     config.hosts << 'choli.org'
+
+    config.autoload_paths += Dir["#{config.root}/app/models/*/"]
+    config.autoload_paths += Dir["#{config.root}/app/resources/*/"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
