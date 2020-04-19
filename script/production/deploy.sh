cf18e64f28059baf1abb7bb806564a5e7bcd5f0f
坪内崇人 <taka.ur.0505@gmail.com>
2020-04-19 10:17:05 +0900

    デプロイ用のscript作成

---
+++ script/production/deploy.sh
@@ -0,0 +1,11 @@
+cd /var/www/html/test_sakura_rails
+## 一応、Source読み込み
+source ~/.bash_profile
+## ファイルダウンロード
+git pull
+## DBをMigrateしてしまう。
+bundle exec rails db:migrate
+## 静的ファイルアップ
+bundle exec rails assets:precompile
+## APサーバー再起動
+bundle exec rails unicorn:restart
