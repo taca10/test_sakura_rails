28b0edef4bd17ab102afc93fc11e8cca107966ff
坪内崇人 <taka.ur.0505@gmail.com>
2020-04-26 17:22:27 +0900

    wearからスクレイピングして、おすすめをslackbotに表示するロジック作成

--- app/models/wear_scraping.rb
+++ app/models/wear_scraping.rb
@@ -1,4 +1,4 @@
-class ZozoScraping
+class WearScraping
   require 'mechanize'
   def self.fetch_wear_page(url=nil)
     agent = Mechanize.new
@@ -6,25 +6,14 @@ class ZozoScraping
     item_snaps = page.search(".like_mark")
 
     category_id = page.search(".coordinate")[0].attributes['data-categoryid'].value
-    sleep(3)
 
     item_imgs = page.search(".image .img img")
-    images = []
-    item_imgs.each.with_index(2) do |item_img, i|
-      images << "https:" + item_img.attributes["data-originalretina"].value
-    end
+    images = item_imgs.map { |item_img| "https:" + item_img.attributes["data-originalretina"].value }
 
-    p 'sucess3'
-
-    hoge = []
     json_url = "https://wear.jp/common/json/coordinate_items.json?snap_ids="
-    item_snaps.each do |item_snap|
-      snap_id = item_snap.attributes["data-snapid"].value
-      json_url += snap_id + ","
-      hoge << snap_id
-    end
-    p 'sucess4'
-    json_url = "#{json_url.chop}&category_id=#{category_id}"
+    snap_id = item_snaps.map{ |item_snap| item_snap.attributes["data-snapid"].value }
+    json_url += snap_id.join(',')
+    json_url = "#{json_url}&category_id=#{category_id}"
 
     uri = URI.parse(json_url)
     json = Net::HTTP.get(uri)
@@ -46,9 +35,9 @@ class ZozoScraping
         end
       end
     end
-    post_texts = post_texts.each_slice(2).to_a
+    post_texts = post_texts.each_slice(1).to_a
 
-    # post_texts.each {|text| p text.join("\n") }
+    post_texts = post_texts[0]
 
     Slack.configure do |config|
       config.token = ENV["SLACK_OAUTH_ACCESS_TOKEN"]
@@ -59,14 +48,9 @@ class ZozoScraping
       client.chat_postMessage(
         as_user: 'true',
         channel: '#zozo_scraping',
-        text: post_text.join("\n")
+        text: post_text
       )
     end
 
-  end
-
-  def fetch_wear_date
-    sleep(3)
-    p 'before_aciton fetch_wear_date'
   end
 end
